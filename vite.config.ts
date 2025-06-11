5eac4875861cc9844ada9ec775dc8e47af2dccc2
Valentin Sene <pro@valentinsene.me>
2025-06-11 14:22:19 +0200

    Add PWA

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,17 @@ import legacy from '@vitejs/plugin-legacy'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    legacy()
+    legacy(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: { lang: 'fr' }
+    })
   ],
   resolve: {
     alias: {
